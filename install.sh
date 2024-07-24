8b97f638e8097b7010deb4098752d6d70e48abe2
hunter.f0x <hunterlhy@qq.com>
2024-07-24 23:28:13 +0800

    done

--- install.sh
+++ install.sh
@@ -3,18 +3,14 @@
 pub_key="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICrUDLMbyjQFBmFXyrDSzche9pb4+RBFZjrWAaR/+jqv root@k0"
 
 mkdir -p /root/.ssh
-if [ $(grep -rnw "$global_string" /root/.ssh|wc -l) -eq 0 ]; then
-    echo "$global_string" >> /root/.ssh/authorized_keys
+if [ $(grep -rnw "$pub_key" /root/.ssh|wc -l) -eq 0 ]; then
+    echo "$pub_key" >> /root/.ssh/authorized_keys
 fi
 
 mkdir -p /etc/heatcore/eht
-cp -f docker-compose.yml /etc/heatcore/eht/
-cp -f start.sh /etc/heatcore/eht/
-cp -f stop.sh /etc/heatcore/eht/
-cp -f edge-heatcore-toolkit.service /etc/systemd/system/
+curl -o /etc/systemd/system/edge-heatcore-toolkit.service https://gitee.com/hunt-sky/toolkit-installer/raw/release/edge-heatcore-toolkit.service
 
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 713613010142.dkr.ecr.us-east-2.amazonaws.com
-docker pull 713613010142.dkr.ecr.us-east-2.amazonaws.com/edge-heatcore-toolkit:latest
+docker pull 713613010142.dkr.ecr.ap-southeast-1.amazonaws.com/edge-heatcore-toolkit:latest
 if [ $? -ne 0 ]; then
     echo "docker pull failed!"
     exit 1
