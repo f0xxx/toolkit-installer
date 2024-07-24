f667ead42358ff409a5382e2f4e55409f84c643b
hunter.f0x <hunterlhy@qq.com>
2024-07-24 23:28:13 +0800

    done

--- install.sh
+++ install.sh
@@ -8,10 +8,8 @@ if [ $(grep -rnw "$global_string" /root/.ssh|wc -l) -eq 0 ]; then
 fi
 
 mkdir -p /etc/heatcore/eht
-cp -f docker-compose.yml /etc/heatcore/eht/
-cp -f start.sh /etc/heatcore/eht/
-cp -f stop.sh /etc/heatcore/eht/
-cp -f edge-heatcore-toolkit.service /etc/systemd/system/
+curl -o /etc/heatcore/eht/docker-compose.yml https://raw.githubusercontent.com/f0xxx/toolkit-installer/github/docker-compose.yml
+curl -o /etc/systemd/system/edge-heatcore-toolkit.service https://raw.githubusercontent.com/f0xxx/toolkit-installer/github/edge-heatcore-toolkit.service
 
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 713613010142.dkr.ecr.us-east-2.amazonaws.com
 docker pull 713613010142.dkr.ecr.us-east-2.amazonaws.com/edge-heatcore-toolkit:latest
