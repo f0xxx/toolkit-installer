d4c6f28ae9dd71126b4a2f37fb6f182e723353b5
hunter.f0x <hunterlhy@qq.com>
2024-07-25 14:30:59 +0800

    speedup repo op

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ mkdir -p /etc/heatcore/eht
 curl -o /etc/heatcore/eht/docker-compose.yml https://raw.githubusercontent.com/f0xxx/toolkit-installer/github/docker-compose.yml
 curl -o /etc/systemd/system/edge-heatcore-toolkit.service https://raw.githubusercontent.com/f0xxx/toolkit-installer/github/edge-heatcore-toolkit.service
 
-aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 713613010142.dkr.ecr.us-east-2.amazonaws.com
-docker pull 713613010142.dkr.ecr.us-east-2.amazonaws.com/edge-heatcore-toolkit:latest
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 713613010142.dkr.ecr.ap-southeast-1.amazonaws.com
+docker pull 713613010142.dkr.ecr.ap-southeast-1.amazonaws.com/edge-heatcore-toolkit:latest
 if [ $? -ne 0 ]; then
     echo "docker pull failed!"
     exit 1
--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+systemctl stop edge-heatcore-toolkit.service
 systemctl disable edge-heatcore-toolkit.service
 
-docker rmi 713613010142.dkr.ecr.us-east-2.amazonaws.com/edge-heatcore-toolkit:latest
+docker rmi 713613010142.dkr.ecr.ap-southeast-1.amazonaws.com/edge-heatcore-toolkit:latest
 
 rm -f /etc/systemd/system/edge-heatcore-toolkit.service
 rm -rf /etc/heatcore/eht
